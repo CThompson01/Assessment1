00ca5e2b848472e63687fbe502cd8e1d3ba11336
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-09-01 08:22:37 -0500

    Initial Commit
    Finish test1, test2, and test3.

---
+++ src/standards/test2.java
@@ -0,0 +1,9 @@
+package standards;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Mr. Brosius is a cool dude");
+	}
+
+}
---
+++ src/standards/test3.java
@@ -0,0 +1,12 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "This is a gui output");
+		//System.out.println("This is a console output");
+	}
+
+}
